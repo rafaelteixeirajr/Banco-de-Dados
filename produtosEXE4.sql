6878924a5e91822afa39a132ef53dca2a383c5aa
rafaelteixeirajr <rafael.moura54@etec.sp.gov.br>
2026-06-10 20:27:34 -0300

    Add files via upload

---
+++ produtosEXE4.sql
@@ -0,0 +1,122 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 11/06/2026 às 01:26
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `produtos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `idcategoria` int(11) NOT NULL,
+  `categoria` varchar(100) NOT NULL,
+  `descricao` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `categoria`
+--
+
+INSERT INTO `categoria` (`idcategoria`, `categoria`, `descricao`) VALUES
+(1, 'produtos de limpeza', 'produtos usados na limpeza da casa'),
+(2, 'produtos de higiene', 'produtos usado para a higiene pessoal');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `itens`
+--
+
+CREATE TABLE `itens` (
+  `idproduto` int(11) NOT NULL,
+  `idcategoria` int(11) DEFAULT NULL,
+  `nomeproduto` varchar(100) NOT NULL,
+  `preco` decimal(10,2) NOT NULL,
+  `qtde` int(11) NOT NULL,
+  `datacad` date DEFAULT NULL,
+  `ativo` bit(1) NOT NULL,
+  `Marca` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `itens`
+--
+
+INSERT INTO `itens` (`idproduto`, `idcategoria`, `nomeproduto`, `preco`, `qtde`, `datacad`, `ativo`, `Marca`) VALUES
+(1, 1, 'Detergente', 2.50, 1, '2026-06-10', b'1', 'Ype'),
+(2, 1, 'Candida', 5.50, 1, '2026-06-10', b'1', 'Candura'),
+(3, 1, 'paha de aço', 4.50, 8, '2026-06-10', b'1', 'Bombril'),
+(4, 1, 'Desinfetante', 5.50, 1, '2026-06-10', b'1', 'Ype'),
+(5, 1, 'Amaciante concentrado', 25.00, 1, '2026-06-10', b'1', 'Downy'),
+(6, 2, 'Sabonete em barra', 3.20, 1, '2026-06-10', b'1', 'Palmolive'),
+(7, 2, 'Pasta de dente', 4.80, 1, '2026-06-10', b'1', 'Colgate'),
+(8, 2, 'Shampoo', 15.90, 1, '2026-06-10', b'1', 'Clear'),
+(9, 2, 'Condicionador', 18.50, 1, '2026-06-10', b'1', 'Clear');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`idcategoria`);
+
+--
+-- Índices de tabela `itens`
+--
+ALTER TABLE `itens`
+  ADD PRIMARY KEY (`idproduto`),
+  ADD KEY `idcategoria` (`idcategoria`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `idcategoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `itens`
+--
+ALTER TABLE `itens`
+  MODIFY `idproduto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `itens`
+--
+ALTER TABLE `itens`
+  ADD CONSTRAINT `itens_ibfk_1` FOREIGN KEY (`idcategoria`) REFERENCES `categoria` (`idcategoria`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
